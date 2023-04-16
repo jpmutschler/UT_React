4723ccd8785a24beec65867302dcea1674cc1f1b
jpmutschler <129906968+jpmutschler@users.noreply.github.com>
2023-04-15 18:10:22 -0600

    Add files via upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "UTTest": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3joshuttest": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
